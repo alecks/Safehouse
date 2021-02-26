d6fec9fdd094c2330458e0c83d0cebcd56b918bb
Alex P <alexeek@protonmail.com>
2021-02-26 21:09:03 +0000

    feat: add passwords page

---
+++ client/src/pages/passwords.tsx
@@ -0,0 +1,13 @@
+export default function () {
+  return (
+    <>
+      <h1>Password Manager</h1>
+      <p>
+        Safehouse allows you to store and manage passwords securely with{" "}
+        <a href="https://bitwarden.com">Bitwarden</a>. It is powered by an
+        open-source implementation written in Rust --{" "}
+        <a href="https://github.com/dani-garcia/bitwarden_rs">bitwarden-rs</a>.
+      </p>
+    </>
+  );
+}
